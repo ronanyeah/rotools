86b281bddab7c8f5603b3985a9f51225c190f526
Rónán <ronan_mccabe@protonmail.com>
2022-05-13 16:47:09 -0500

    0.2.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,51 @@
-#[derive(serde::Deserialize, Debug)]
-struct HasuraError {
-    error: HasuraInfo,
+use anyhow::anyhow;
+use serde::de::value::MapDeserializer;
+use serde::Deserialize;
+use std::fmt;
+
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct HasuraInternalError {
+    pub error: HasuraInternal,
 }
 
-#[derive(serde::Deserialize, Debug)]
-struct HasuraInfo {
-    description: Option<String>,
-    exec_status: String,
-    hint: Option<String>,
-    message: String,
-    status_code: String,
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct HasuraErrors {
+    pub hasura_errors: Vec<ParsedError>,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct ParsedError {
+    pub message: String,
+    pub error: Option<HasuraInfo>,
+}
+
+// I think for postgres errors
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct HasuraInternal {
+    pub description: Option<String>,
+    pub exec_status: String,
+    pub hint: Option<String>,
+    pub message: String,
+    pub status_code: String,
+}
+
+// https://github.com/hasura/graphql-engine/blob/b6eb71ae07ed72965db51ed6a15af55f70730324/server/src-lib/Hasura/Base/Error.hs#L178
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct HasuraInfo {
+    pub code: String,
+    pub path: String,
+    pub error: Option<String>,
+    pub internal: Option<HasuraInternalError>,
+}
+
+impl fmt::Display for HasuraErrors {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            serde_json::to_string_pretty(self).map_err(|_| fmt::Error)?
+        )
+    }
 }
 
 #[cfg(test)]
@@ -20,61 +56,55 @@ mod tests {
     }
 }
 
-/// # Errors
-///
-/// [GraphQL Spec](http://spec.graphql.org/draft/#sec-Errors)
 pub async fn graphql_request<D: serde::de::DeserializeOwned>(
     req: reqwest::RequestBuilder,
-) -> Result<D, Vec<String>> {
-    let data = req.send().await.map_err(|e| vec![format!("{:?}", e)])?;
+) -> anyhow::Result<graphql_client::Response<D>> {
+    let data = req.send().await?;
 
-    let decode: reqwest::Result<graphql_client::Response<D>> = data.json().await;
+    let decode: graphql_client::Response<D> = data.json().await?;
 
-    let body = decode.map_err(|e| vec![format!("{:?}", e)])?;
+    Ok(decode)
+}
 
+pub async fn graphql_parse<D: serde::de::DeserializeOwned>(
+    body: graphql_client::Response<D>,
+) -> anyhow::Result<D> {
     match body.data {
         Some(d) => Ok(d),
-        None => Err(match body.errors {
-            Some(errors) => parse_errors(errors),
-            None => vec!["hasura missing data".to_string()],
-        }),
+        None => match body.errors {
+            Some(errors) => {
+                let xs: Vec<_> = errors.into_iter().map(parse_error).collect();
+                let ctx = HasuraErrors { hasura_errors: xs };
+                Err(anyhow!(ctx))
+            }
+            None => Err(anyhow!("Hasura: No data or errors")),
+        },
     }
 }
 
-/// # Errors
-///
-/// [GraphQL Spec](http://spec.graphql.org/draft/#sec-Errors)
 pub async fn hasura_request<T: serde::Serialize, D: serde::de::DeserializeOwned>(
     client: &reqwest::Client,
     graphql_endpoint: &url::Url,
     hasura_admin_secret: &str,
     body: T,
-) -> Result<D, Vec<String>> {
+) -> anyhow::Result<D> {
     let req = client
         .post(graphql_endpoint.as_str())
         .header("x-hasura-admin-secret", hasura_admin_secret.as_bytes())
         .json(&body);
 
-    graphql_request(req).await
+    let res = graphql_request(req).await?;
+
+    graphql_parse(res).await
 }
 
-fn parse_errors(errors: Vec<graphql_client::Error>) -> Vec<String> {
-    errors
-        .into_iter()
-        .map(|e| {
-            let internal = e
-                .extensions
-                .as_ref()
-                .and_then(|ext| ext.get("internal"))
-                .and_then(|v| serde_json::from_value::<HasuraError>(v.clone()).ok())
-                .map(|x| x.error);
+pub fn parse_error(e: graphql_client::Error) -> ParsedError {
+    let internal = e.extensions.as_ref().and_then(|ext| {
+        HasuraInfo::deserialize(MapDeserializer::new(ext.clone().into_iter())).ok()
+    });
 
-            match internal {
-                Some(h_err) => {
-                    format!("{}\n{:?}", e.message, h_err)
-                }
-                None => e.message,
-            }
-        })
-        .collect()
+    ParsedError {
+        message: e.message,
+        error: internal,
+    }
 }
